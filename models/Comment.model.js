4a00bed630ee9c4aa7d44f120fb1da7f2c44ff0b
Jino Hong <jinohong1993@gmail.com>
2022-07-23 21:28:17 -0300

    updated backend server 95%

--- models/Comment.model.js
+++ models/Comment.model.js
@@ -2,7 +2,7 @@ const { Schema, model, Types } = require("mongoose");
 
 const commentSchema = new Schema({
   owner: { type: Types.ObjectId, ref: "User" },
-  post: { type: Types.ObjectId, ref: "Post" },
+  post: [{ type: Types.ObjectId, ref: "Post" }],
   comment: { type: String, required: true, minlength: 64 },
   likes: [{ type: Types.ObjectId, ref: "User" }],
 });
